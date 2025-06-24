d715a765910a06abd077bd62864fc7ff9bba490c
Anthony Thomas <antmoth@macc-bastion-190.umdl.umich.edu>
2025-06-24 11:11:01 -0400

    clean up better

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+rm ice-kernel-dkms*
 cd build/
-rm -rf *.deb *.tar.gz
+rm -rf *.tar.gz src/
 
 mkdir src/
 curl -OL https://github.com/intel/ethernet-linux-ice/releases/download/v1.17.4/ice-1.17.4.tar.gz
