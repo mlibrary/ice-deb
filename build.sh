77f58841fa27449435bdadeaba422d6e6bfcd136
Anthony Thomas <antmoth@macc-bastion-190.umdl.umich.edu>
2025-06-24 10:46:59 -0400

    working package build?

--- build.sh
+++ build.sh
@@ -2,11 +2,10 @@
 
 ROOTDIR='ice_1.17.4_all'
 
-rf -rf *.deb *.tar.gz
+rm -rf *.deb *.tar.gz
 
-mkdir -p ${ROOTDIR}/usr/src/
-cp -a DEBIAN ${ROOTDIR}
+mkdir src/
 curl -OL https://github.com/intel/ethernet-linux-ice/releases/download/v1.17.4/ice-1.17.4.tar.gz
-tar xf ice-1.17.4.tar.gz -C ${ROOTDIR}/usr/src
-dpkg-deb -Zxz --build --root-owner-group ${ROOTDIR}
-
+tar xf ice-1.17.4.tar.gz -C src
+# dpkg-deb -Zxz --build --root-owner-group ${ROOTDIR}
+dpkg-buildpackage -us -uc
