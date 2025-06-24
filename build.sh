d737ad31c0fae26667a5ec54c58ce5c090324a4b
Anthony Thomas <antmoth@macc-bastion-190.umdl.umich.edu>
2025-06-24 10:57:16 -0400

    make a build dir so artifacts end up in parent dir

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-ROOTDIR='ice_1.17.4_all'
-
+cd build/
 rm -rf *.deb *.tar.gz
 
 mkdir src/
 curl -OL https://github.com/intel/ethernet-linux-ice/releases/download/v1.17.4/ice-1.17.4.tar.gz
 tar xf ice-1.17.4.tar.gz -C src
-# dpkg-deb -Zxz --build --root-owner-group ${ROOTDIR}
 dpkg-buildpackage -us -uc
