9cbefd809428708d16e73b7ca4b4c318859175d0
Anthony Thomas <antmoth@macc-bastion-190.umdl.umich.edu>
2025-06-24 22:12:59 -0400

    it works?

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ rm ice-kernel-dkms*
 cd build/
 rm -rf *.tar.gz src/
 
-mkdir src/
 curl -OL https://github.com/intel/ethernet-linux-ice/releases/download/v1.17.4/ice-1.17.4.tar.gz
-tar xf ice-1.17.4.tar.gz -C src
+tar xf ice-1.17.4.tar.gz
+mv ice-1.17.4 src/
 dpkg-buildpackage -us -uc
